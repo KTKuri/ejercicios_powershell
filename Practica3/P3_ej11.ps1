0d89cecdd54976b81ac64f6a9cf94a591f9759bd
KTKuri <eperezigarcia@gmail.com>
2023-09-26 13:53:11 +0200

    ç

---
+++ Practica3/P3_ej11.ps1
@@ -0,0 +1,14 @@
+﻿[int]$edad = Read-Host "Introduce tu edad"
+
+If ($edad -lt 4){
+    Write-Host "¡Entrada gratis para menores de 4 años!"
+
+}ElseIf ($edad -ge 4 -and $edad -lt 18){
+    Write-Host "El precio de la entrada es 5€"
+
+}ElseIf ($edad -gt 18){
+    Write-Host "El precio de la entrada es de 10€"
+
+}Else {
+    Write-Host "Edad introducida incorrecta"
+}
---
+++ Practica3/P3_ej12.ps1
@@ -0,0 +1,47 @@
+﻿Clear-Host
+$tipopizza = Read-Host "Bienvenido a nuestra pizzería. Tenemos 2 tipos de pizza (Vegetariana o No Vegetariana). ¿Desea que la pizza sea vegetariana?
+
+1. Si
+2. No
+
+Elija una de las 2 opciones"
+
+If ($tipopizza -eq 1){
+
+    $ingreveg = Read-Host "Todas nuestras pizzas llevan tomate y mozarella, pero además usted puede añadir un ingrediente extra. ¿Desea añadir [Pimiento] o [Tofu]?"
+
+        If ($ingreveg -eq "Pimiento" -or $ingreveg -eq "Tofu"){
+            Write-Host "Ingredientes finales de la pizza vegetariana:
+
+            -Mozarella (obligatorio)
+            -Tomate (obligatorio)
+            -$ingreveg (elegido)
+
+            La preparación de la pizza empezará enseguida."
+
+        }Else {
+            Write-Host "Ingrediente introducido erróneo. Por favor, introduzca [Pimiento] o [Tofu]"
+        }
+    
+
+}ElseIf ($tipopizza -eq 2){
+
+    $ingrenoveg = Read-Host "Todas nuestras pizzas llevan tomate y mozarella, pero además usted puede añadir un ingrediente extra. ¿Desea añadir [Peperoni], [Jamón] o [Salmón]?"
+
+        If ($ingrenoveg -eq "Peperoni" -or $ingreveg -eq "Jamón" -or $ingrenoveg -eq "Salmón"){
+            Write-Host "Ingredientes finales de la pizza no vegetariana:
+
+            -Mozarella (obligatorio)
+            -Tomate (obligatorio)
+            -$ingrenoveg (elegido)
+
+            La preparación de la pizza empezará enseguida."
+
+        }Else {
+            Write-Host "Ingrediente introducido erróneo. Por favor, introduzca [Peperoni], [Jamón] o [Salmón]"
+        }
+
+}Else {
+    Write-Host "Valor introducido incorrecto"
+}
+
