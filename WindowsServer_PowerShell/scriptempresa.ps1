cc53fc155ebdd7eeecbc0554594146056d917de2
KTKuri <eperezigarcia@gmail.com>
2023-10-09 20:01:42 +0200

    Carpeta de scripts Powershell

---
+++ WindowsServer_PowerShell/scriptempresa.ps1
@@ -0,0 +1,16 @@
+﻿#Creación de la Unidad Organizativa y su estructura
+$departamentos = Import-Csv C:\Users\Administrador\Desktop\archivoscsv\departamentos.csv -Delimiter ";"
+$empleados = Import-Csv C:\Users\Administrador\Desktop\archivoscsv\empleados.csv -Delimiter ";"
+
+New-ADOrganizationalUnit -Name "Empresa" -Path "DC=EMPRESA,DC=LOCAL" -Description "Unidad Empresa"
+
+foreach ($dep in $departamentos) 
+{
+    New-ADOrganizationalUnit -Name $($dep.departamento) -Path "OU=Empresa,DC=EMPRESA,DC=LOCAL" -Description "Unidad Empresa"
+    New-ADGroup -Name $($dep.departamento) -GroupCategory Security -GroupScope Global -Path "OU=$($dep.departamento),OU=Empresa,DC=EMPRESA,DC=LOCAL"
+}
+
+foreach ($emp in $empleados)
+{
+    New-ADUser -Name $($emp.nombre) -Path "OU=$($emp.departamento),OU=Empresa,DC=Empresa,DC=Local" -SamAccountName "$($emp.nombre)$($emp.apellido)" -UserPrincipalName "$($emp.nombre)$($emp.apellido)@empresa.local" -AccountPassword (ConvertTo-SecureString "aso2021." -AsPlainText -Force) -GivenName "$($emp.nombre)" -Surname "$($emp.apellido)" -ChangePasswordAtLogon $true -Enabled $true
+}
