1ffefdbef6e48e9a5f06058fdda44d67c8273f1c
KTKuri <eperezigarcia@gmail.com>
2023-10-19 09:57:39 +0200

    Pruebas ACL

---
+++ WindowsServer_PowerShell/Pruebas/PruebaACL1.ps1
@@ -0,0 +1,28 @@
+﻿#Obtener una variable y acceder a ella en un formato específico
+$acl = Get-Acl -Path C:\ASO\prueba
+$acl.Access | Format-Table
+
+#Deshabilitar la herencia ([true] para habilitar/ [false] para desactivar)
+#Preservar las reglas de acceso ([true] para preservar/ [false] para quitar)
+#$acl.SetAccessRuleProtection($true, $true)
+
+#Aplicar cambios
+#Set-Acl -Path C:\ASO\prueba -AclObject $acl
+#O también:
+#$acl | Set-Acl -Path C:\ASO\prueba
+
+$listaACE = $acl.Access
+
+#Eliminar reglas de acceso
+foreach ($ace in $listaACE)
+{
+    $ace | Format-List
+    $acl.RemoveAccessRule($ace)
+}
+
+#Añadir grupo Administradores tenga Control Total
+$permiso = 'Administradores','FullControl','ContainerInherit,ObjectInherit','None','Allow'
+$ace = New-Object -TypeName System.Security.AccessControl.FileSystemAccessRule -ArgumentList $permiso
+$acl.SetAccessRule($ace)
+
+Set-Acl -Path C:\ASO\prueba -AclObject $acl
---
+++ WindowsServer_PowerShell/Pruebas/PruebaACL2.ps1
@@ -0,0 +1,18 @@
+﻿$acl = Get-Acl -Path C:\ASO\prueba2 
+
+#Deshabilitar la herencia y eliminar TODAS las reglas de acceso
+$acl.SetAccessRuleProtection($true, $false)
+
+#Añadimos al grupo de Administradores el permiso de Control Total
+$permiso = 'Administradores','FullControl','ContainerInherit,ObjectInherit','None','Allow'
+$permiso2 = 'Usuarios del dominio','Read','ContainerInherit,ObjectInherit','None','Allow'
+
+$ace = New-Object -TypeName System.Security.AccessControl.FileSystemAccessRule -ArgumentList $permiso
+$acl.SetAccessRule($ace)
+
+
+$ace2 = New-Object -TypeName System.Security.AccessControl.FileSystemAccessRule -ArgumentList $permiso2
+$acl.SetAccessRule($ace2)
+$ace2 | Format-Table
+
+Set-Acl -Path C:\ASO\prueba2 -AclObject $acl
