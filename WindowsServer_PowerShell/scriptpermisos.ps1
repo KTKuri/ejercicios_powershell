a73ac206185635a59b6a99cc756aa9c472b4386d
KTKuri <eperezigarcia@gmail.com>
2023-10-23 09:24:52 +0200

    Script de permisos acabado

--- WindowsServer_PowerShell/scriptpermisos.ps1
+++ WindowsServer_PowerShell/scriptpermisos.ps1
@@ -1,7 +1,6 @@
 ﻿#Crear la carpeta principal
 New-Item -Name Empresa -Path C:\ -ItemType Directory
 
-
 #Crear la estructura
 $departamentos = Import-Csv C:\Users\Administrador\Desktop\archivoscsv\departamentos.csv -Delimiter ";"
 
@@ -18,4 +17,25 @@ foreach ($dep in $departamentos)
     Grant-SmbShareAccess -Name $($dep.departamento) -AccountName "Administrador" -AccessRight Full -Force
     Grant-SmbShareAccess -Name $($dep.departamento) -AccountName $($dep.departamento) -AccessRight Change -Force
     Grant-SmbShareAccess -Name $($dep.departamento) -AccountName "Usuarios del dominio" -AccessRight Read -Force
+}
+
+#Asignar permisos NTFS a Empresa
+foreach($dep in $departamentos)
+{
+    $acl = Get-Acl -Path C:\Empresa\$($dep.departamento)
+    $acl.SetAccessRuleProtection($true,$false)
+
+    $permisousdom = 'Usuarios del dominio','Read','ContainerInherit,ObjectInherit','None','Allow'
+    $permisodep = "$($dep.departamento)",'Modify','ContainerInherit,ObjectInherit','None','Allow'
+    $permisoadmin = 'Administradores','FullControl','ContainerInherit,ObjectInherit','None','Allow'
+
+    $ace1 = New-Object System.Security.AccessControl.FileSystemAccessRule -ArgumentList $permisousdom
+    $acl.SetAccessRule($ace1)
+    $ace2 = New-Object System.Security.AccessControl.FileSystemAccessRule -ArgumentList $permisodep
+    $acl.SetAccessRule($ace2)
+    $ace3 = New-Object System.Security.AccessControl.FileSystemAccessRule -ArgumentList $permisoadmin
+    $acl.SetAccessRule($ace3)
+    $ace | Format-Table
+
+    Set-Acl -AclObject $acl -Path C:\Empresa\$($dep.departamento)
 }
