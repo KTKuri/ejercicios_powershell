108a05c1b5d6702e89a0d25e1480f7201ff07fbe
KTKuri <eperezigarcia@gmail.com>
2023-10-30 09:45:45 +0100

    Acabada y subida la práctica 7 (perfiles móviles)

---
+++ WindowsServer_PowerShell/UT6_P7_Perfiles_moviles.ps1
@@ -0,0 +1,12 @@
+﻿#Previo
+Clear-Host
+$empleados = Import-Csv C:\Users\Administrador\Desktop\archivoscsv\empleados.csv -delimiter ";"
+
+
+foreach ($emp in $empleados)
+
+{
+    If ($emp.departamento -eq "PERSONAL"){
+        Set-ADUser -Identity "$($emp.nombre).$($emp.apellido)" -ProfilePath "\\empresa-DC1\Empresa_users$\$($emp.nombre).$($emp.apellido)"
+    }
+}
