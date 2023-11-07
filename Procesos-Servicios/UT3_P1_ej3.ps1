13fca9117843f82035d606d525f556e0841b42ec
KTKuri <eperezigarcia@gmail.com>
2023-11-07 12:30:26 +0100

    Práctica 1 terminada

---
+++ Procesos-Servicios/UT3_P1_ej3.ps1
@@ -0,0 +1,4 @@
+﻿#Matar todos los procesos de un usuario solicitado por al administrador
+$usuario = Read-Host "Elija un usuario para cerrar todos sus procesos"
+
+Get-Process -IncludeUserName | where UserName -eq "EMPRESA\$usuario" | Stop-Process -Force
